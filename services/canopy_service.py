7102d5aa4ceec25097645209110a1ba275e04a83
Clayexe <77508458+Clayexe@users.noreply.github.com>
2026-08-06 18:17:07 -0400

    Pass canopy raster path to sample_canopy_cover

--- services/canopy_service.py
+++ services/canopy_service.py
@@ -2,6 +2,9 @@ from analytics.canopy import sample_canopy_cover, calculate_throughfall
 
 
 class CanopyService:
+    def __init__(self, canopy_raster_path=None):
+        self.canopy_raster_path = canopy_raster_path
+
     def sample(self, lat, lon, precipitation_probability=0):
-        canopy_cover = sample_canopy_cover(lat, lon)
+        canopy_cover = sample_canopy_cover(lat, lon, self.canopy_raster_path)
         return calculate_throughfall(canopy_cover, precipitation_probability)
